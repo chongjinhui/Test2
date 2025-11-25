b23c557027f28aa20acae7507c17125fb56ae5f1
chongjinhui <chongjinhui@yahoo.com>
2025-11-25 23:25:11 +0800

    Update Practical1.java

--- Practical1.java
+++ Practical1.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         ShoppingCart cart = new ShoppingCart();
         
-        Product apple = new Product("Apple", 1.0);
+        Product apple = new Product("Apple", 1.0); //David
         Product banana = new Product("Banana", 0.5);
 
         cart.addProduct(apple, 3);
