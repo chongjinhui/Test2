ed06b2eba6dcbefd17b378514e6985b92b8c41a9
chongjinhui <chongjinhui@yahoo.com>
2025-11-25 23:17:14 +0800

    Initial commit

---
+++ Practical1.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+        
+        Product apple = new Product("Apple", 1.0);
+        Product banana = new Product("Banana", 0.5);
+
+        cart.addProduct(apple, 3);
+        cart.addProduct(banana, 2);
+
+        System.out.println("Total before removal: " + cart.calculateTotal());
+
+        cart.removeProduct(apple);
+        
+        System.out.println("Total after removal: " + cart.calculateTotal());
+    }
+}
